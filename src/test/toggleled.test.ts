c74bb6468b18d27a428d6724fec471b6bd86bed7
Samson <zhsamson12@gmail.com>
2025-09-15 13:44:27 -0400

    New Led Test

---
+++ src/test/toggleled.test.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from "vitest";
+import { ledIsOn } from "../togglels";
+
+describe("isLedOn", () => {
+  it("returns true if light is on", () => {
+    expect(ledIsOn(1)).toBe(true);
+  });
+  it("returns false if light is off", () => {
+    expect(ledIsOn(0)).toBe(false);
+  });
+
+});
